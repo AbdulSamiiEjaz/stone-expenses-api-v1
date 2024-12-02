5bf0f7728a0303464bc14fceba83b01d6023ee91
AbdulSamiEjaz <coderthe470@gmail.com>
2024-12-02 14:14:12 +0500

    Added Required Dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia({
+  prefix: "/v1",
+})
+  .use(
+    swagger({
+      path: "/swagger",
+      documentation: {
+        info: {
+          title: "Stone Expenses API",
+          version: "1.0.0",
+        },
+      },
+    })
+  )
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}/v1/swagger`
 );
